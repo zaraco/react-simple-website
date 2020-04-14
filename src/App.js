6d3d089dbcb4ba1315db803d47a7a4956ac83d4c
Zaraco <zahrateymouri90@gmail.com>
2020-04-14 14:15:49 +0200

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {Container, Row, Col, Carousel, Navbar, Nav, NavDropdown, Form, FormControl, Button} from "react-bootstrap";
+import {BrowserRouter as Router, Route, Switch,} from 'react-router-dom'
 import Home from "./Component/Home";
 import About from "./Component/About";
 import Contact from "./Component/Contact";
-import {Col, Container, Row} from "react-bootstrap";
 
 class App extends Component {
     constructor() {
@@ -18,8 +18,61 @@ class App extends Component {
                 <Container>
                     <Row>
                         <Col>
+                            <Navbar bg="light" expand="lg">
+                                <Navbar.Brand href="/home">Power Component</Navbar.Brand>
+                                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav className="mr-auto">
+                                        <Nav.Link href="/home" >Home</Nav.Link>
+                                        <Nav.Link href="/About">About</Nav.Link>
+                                        <Nav.Link href="/Contact">Contact</Nav.Link>
+                                        <NavDropdown title="Products" id="basic-nav-dropdown">
+                                            <NavDropdown.Item href="#">MV Cable</NavDropdown.Item>
+                                            <NavDropdown.Item href="#">Cable Tray</NavDropdown.Item>
+                                            <NavDropdown.Item href="#">Tower Component</NavDropdown.Item>
+                                            <NavDropdown.Divider/>
+                                            <NavDropdown.Item href="#">MV Switchgear</NavDropdown.Item>
+                                        </NavDropdown>
+                                    </Nav>
+                                    <Form inline>
+                                        <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
+                                        <Button variant="outline-success">Search</Button>
+                                    </Form>
+                                </Navbar.Collapse>
+                            </Navbar>
+                            <br/>
+
+                            <Carousel>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-75"
+                                        src="download (1).jpeg"
+                                        alt="First Refinery"
+                                    />
+
+                                </Carousel.Item>
+
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-75"
+                                        src="download (2).jpeg"
+                                        alt="Second Refinery"
+                                    />
+
+                                </Carousel.Item>
+
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-75"
+                                        src="download (3).jpeg"
+                                        alt="Third Refinery"
+                                    />
+
+                                </Carousel.Item>
+                            </Carousel>
                             <Router>
                                 <Switch>
+                                    <Route path="/"/>
                                     <Route path="/Home" component={Home}/>
                                     <Route path="/About" component={About}/>
                                     <Route path="/Contact" component={Contact}/>
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import {Carousel} from "react-bootstrap";
 
 class Home extends Component {
     constructor() {
@@ -9,34 +8,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <Carousel>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-75"
-                            src="download (1).jpeg"
-                            alt="First Refinery"
-                        />
 
-                    </Carousel.Item>
-
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-75"
-                            src="download (2).jpeg"
-                            alt="Second Refinery"
-                        />
-
-                    </Carousel.Item>
-
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-75"
-                            src="download (3).jpeg"
-                            alt="Third Refinery"
-                        />
-
-                    </Carousel.Item>
-                </Carousel>
             </div>
         );
     }
